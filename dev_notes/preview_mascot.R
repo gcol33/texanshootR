5f5c8a7bf706ec4f24e676044bfa44265f1ee9b3
Gilles Colling <gilles.colling051@gmail.com>
2026-05-10 16:38:41 +0200

    Untrack dev_notes/ scratch scripts
    These were untracked locally; got swept in by an over-broad git add -A
    in the previous commit. Restore the previous local-only convention.

--- dev_notes/preview_mascot.R
+++
@@ -1,7 +0,0 @@
-suppressWarnings(devtools::load_all(quiet = TRUE))
-states <- c("composed", "uncertain", "worried", "anxious",
-            "panicked", "desperate", "resolved")
-for (s in states) {
-  cat("---", s, "---\n", sep = " ")
-  cat(paste(texanshootR:::mascot_render(s, tick = 0L), collapse = "\n"), "\n\n", sep = "")
-}
--- dev_notes/run_tests.R
+++
@@ -1,5 +0,0 @@
-suppressPackageStartupMessages({
-  devtools::load_all(".", quiet = TRUE)
-})
-res <- testthat::test_dir("tests/testthat",
-                          reporter = testthat::SummaryReporter$new())
